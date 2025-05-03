9fe1d30864d52e34d69f06afab0912c5cd197764
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-03 11:10:53 -0300

    Add Anotaçoes Iniciais do Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+/**
+ *
+ * @OA\Info( version="1.0", title="Documentação da API XXX", description="Essa Documentação do projeto XXX, que possui as principais caracteristicas ..." )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Local"
+ * )
+ * @OA\Server(
+ *     url="https://qat.saviorenato.com.br",
+ *     description="QA"
+ * )
+ *
+ */
 abstract class Controller
 {
     //
