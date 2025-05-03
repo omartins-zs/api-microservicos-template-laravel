3f9a13179401ce7f79b5953abbef069cbe80db1d
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-03 11:13:09 -0300

    Add Anotaçoes do Swagger para Documentar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * @OA\Tag(
+     *     name="User",
+     *     description="Informações de usuário"
+     * )
+     *
+     * @OA\Get(
+     *     path="/user",
+     *     summary="Retorno de usuário Teste",
+     *     tags={"User"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Usuário Teste",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 @OA\Property(property="name", type="string", example="Savio Pereira"),
+     *                 @OA\Property(property="type_id", type="integer", example=1),
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function index(Request $request)
+    {
+        return response()->json([
+            'message' => 'Welcome to the API',
+            'data' => $request
+        ]);
+    }
 }
